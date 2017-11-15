849fa42bece528775c94061c9acd61bff59a2645
matt <mws262@cornell.edu>
2017-11-15 10:09:05 -0500

    testing
    Former-commit-id: ddacfbf28d152dbdf526345e9269d329e20dcd2e
    Former-commit-id: 8db6db2f5cb498aab7b41c4fc221011e0e9d1862

--- src/python/Test_Load.py
+++ src/python/Test_Load.py
@@ -1,13 +1,25 @@
 import densedata_pb2
 import sys
+import timeit
+
 
 
 dataset = densedata_pb2.DataSet()
 try:
+	start = timeit.timeit();
 	f = open("../../denseData_2017-11-06_08-53-50.proto","rb")
+	end = timeit.timeit()
+	print("Time to open binary: " + str(end - start) + " seconds")
+
+	start = timeit.timeit();
 	dataset.ParseFromString(f.read())
+	end = timeit.timeit()
+	print("Time to parse binary: " + str(end - start) + " seconds")
 	f.close
 except IOError:
-	print "BAD"
+	print "Import failed"
+
+for runs in dataset.denseData:
+	print(len(runs.state))
 
 print(dataset.denseData[0].state[0].body.y)
