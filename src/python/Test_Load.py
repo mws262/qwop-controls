51f11158052334a999dd3cf16bc05e705c1d83da
Matt Sheen <mws262@cornell.edu>
2017-11-15 16:27:38 -0500

    Processing data in python. Figured out how to load protobuf w cpp. 50x faster
    Former-commit-id: 05d84467298514a94bd0c9a9f0dfd79d564678a1
    Former-commit-id: b747bada1a5f03ffc9ddb36972d9f4e67b3ee765
    Former-commit-id: dd08a6bf16a4151360c08e96a2b48a32f7d358bd

--- src/python/Test_Load.py
+++ src/python/Test_Load.py
@@ -1,25 +1,51 @@
-import densedata_pb2
-import sys
+import densedata_pb2 as dataset
 import timeit
+#import tensorflow as tf
+import numpy as np
+import matplotlib as mat
+import matplotlib.pyplot as plt
+import pylab
 
 
-
-dataset = densedata_pb2.DataSet()
+dataset = dataset.DataSet()
 try:
-	start = timeit.timeit();
-	f = open("../../denseData_2017-11-06_08-53-50.proto","rb")
-	end = timeit.timeit()
-	print("Time to open binary: " + str(end - start) + " seconds")
+    f = open("../../denseData_2017-11-06_08-58-03.proto","rb")
 
-	start = timeit.timeit();
-	dataset.ParseFromString(f.read())
-	end = timeit.timeit()
-	print("Time to parse binary: " + str(end - start) + " seconds")
-	f.close
+    start = timeit.default_timer()
+    dataset.ParseFromString(f.read())
+    end = timeit.default_timer()
+    print("Time to parse binary: " + str(end - start) + " seconds")
+    f.close
 except IOError:
 	print "Import failed"
 
-for runs in dataset.denseData:
-	print(len(runs.state))
+# for runs in dataset.denseData:
+#     print(len(runs.state))
+#
+# print(dataset.denseData[0].state[0].body.y)
 
-print(dataset.denseData[0].state[0].body.y)
+bodyX = np.empty(shape=(len(dataset.denseData[0].state), 1), dtype=np.float32)
+bodyY = np.empty(shape=(len(dataset.denseData[0].state), 1), dtype=np.float32)
+bodyTh = np.empty(shape=(len(dataset.denseData[0].state), 1), dtype=np.float32)
+
+
+for idx, s in enumerate(dataset.denseData[0].state):
+    print str(idx) + "," + str(s.body.x)
+
+    np.put(bodyX, ind=idx, v=s.body.x)
+    np.put(bodyY, ind=idx, v=s.body.y)
+    np.put(bodyTh, ind=idx, v=s.body.th)
+
+
+for i in bodyX:
+    print i
+plt.plot(np.linspace(1,10,np.size(bodyY,0)),bodyX)
+plt.plot(np.linspace(1,10,np.size(bodyY,0)),bodyY)
+pylab.show()
+
+print bodyX[0]
+#print bodyY[0]
+
+print bodyX[-1]
+#print bodyY[-1]
+
