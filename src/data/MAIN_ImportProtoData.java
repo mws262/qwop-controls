eed96247852858bbfb9aa9bd2c94d30a1ef510cd
Matt Sheen <mws262@cornell.edu>
2017-11-21 13:01:11 -0500

    Fixed indexing problem in numpy
    Former-commit-id: fcd4bef34f80f1a5ef5b30ad29ff757988c849d7
    Former-commit-id: e400cea527eced5e0f6ace68eddb1a3d4246318b

---
+++ src/data/MAIN_ImportProtoData.java
@@ -0,0 +1,28 @@
+package data;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import data.DenseDataProtos.DataSet;
+import data.DenseDataProtos.DataSet.DenseData;
+
+public class MAIN_ImportProtoData {
+
+	public static void main(String[] args) {
+		String filename = "denseData_2017-11-06_08-58-03.proto";
+		DataSet dataValidate = null;
+		try {
+			FileInputStream fIn = new FileInputStream(filename);
+			
+			dataValidate = DataSet.parseFrom(new FileInputStream(filename));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		DenseData.State state1 = dataValidate.getDenseData(0).getState(0);
+		float th1 = state1.getHead().getX();
+		DenseData.State state2 = dataValidate.getDenseData(10).getState(0);
+		float th2 = state2.getBody().getTh();
+		System.out.println(th1+ ", " + th2);
+	}
+}
