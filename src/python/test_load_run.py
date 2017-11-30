f67bf0a5e04b451e5b0dcd20ebdb63510a9a1746
Matt Sheen <mws262@cornell.edu>
2017-11-29 22:05:26 -0500

    working converter to tfrecords
    Former-commit-id: 8d1ace51f0d8bf56bd57fd9ee574bc83a0468951
    Former-commit-id: eb3c944c1d58dccea1b37d3e199b5d3fa600b8ff

---
+++ src/python/test_load_run.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import numpy as np
+
+export_dir = './models/'
+# loading
+with tf.Session(graph=tf.Graph()) as sess:
+    tf.saved_model.loader.load(sess, ["tag"], export_dir)
+    graph = tf.get_default_graph()
+    for i in tf.get_default_graph().get_operations():
+        print i.name
+
+    x = graph.get_tensor_by_name("input/x-input:0")
+    dropout = graph.get_tensor_by_name("dropout/dropout_keep_probability:0")
+    model = graph.get_tensor_by_name("layer3/Wx_plus_b/pre_activations:0")
+    (sess.run(model, {x: np.reshape(np.arange(72,dtype=np.float32),[1,72]), dropout: 1}))
