6706e535e6e50b7dd51a2ca20b2d2d436b94d826
matt <mws262@cornell.edu>
2018-01-11 18:07:36 -0500

    Working on TFREcords shit
    Former-commit-id: a2599b25fd36f8a55cded4e1aaa9c00d3e8d64be
    Former-commit-id: 438a14df257ccf8f8b5a47579935b81d28b763f2
    Former-commit-id: 7ea3bcb583bdca3f86ce190a12c056e05f94fb4a

--- src/python/Test_Java_TFRecord.py
+++ src/python/Test_Java_TFRecord.py
@@ -23,23 +23,49 @@ def read_and_decode_single_example(filename):
     # One needs to describe the format of the objects to be returned
     features = tf.parse_single_example(
         serialized_example,
-        feature_lists={
+        features={
             # We know the length of both fields. If not the
             # tf.VarLenFeature could be used
-            'x': tf.FixedLenFeature([72], tf.float32),
-            'y': tf.FixedLenFeature([1], tf.float32)
+            'x': tf.VarLenFeature(tf.float32)
         })
 
 
     # now return the converted data
     x = features['x']
-    y = features['y']
-    return x, y
+    return x
 
+   # example proto decode
+def _parse_function(example_proto):
+    keys_to_features = {'x':tf.FixedLenFeature((1), tf.float32)}
+    parsed_features = tf.parse_single_example(example_proto, keys_to_features)
+    return parsed_features['x']
 
+filenames = ['../../test.NEWNEWNEW']
+dataset = tf.contrib.data.TFRecordDataset(filenames)
 
+# Parse the record into tensors.
+dataset = dataset.map(_parse_function)
 
+# Shuffle the dataset
+dataset = dataset.shuffle(buffer_size=100)
 
+# Repeat the input indefinitly
+dataset = dataset.repeat()
+
+# Generate batches
+dataset = dataset.batch(1)
+
+# Create a one-shot iterator
+iterator = dataset.make_one_shot_iterator()
+
+# Get batch X and y
+x = iterator.get_next()
+print x
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    val = sess.run(x)
+    print val
 # filename_list = []
 # print os.listdir('../../')
 # for file in os.listdir('../../'):
@@ -50,12 +76,15 @@ def read_and_decode_single_example(filename):
 #
 # print filename_list
 # print('%d files in queue.' % len(filename_list))
-
-x_loaded, y_loaded = read_and_decode_single_example(['../../denseData_2017-11-06_08-57-41.NEWNEWNEW', '../../denseData_2017-11-06_08-57-41.NEWNEWNEW'])
-print x_loaded
-with tf.Session() as sess:
-    # ... init our variables, ...
-    sess.run(tf.global_variables_initializer())
-    tf.train.start_queue_runners(sess=sess)
-    x_input, y_input = sess.run([x_loaded, y_loaded])
-    print(x_input)
+#
+# options = tf.python_io.TFRecordOptions(tf.python_io.TFRecordCompressionType.NONE)
+#
+#
+# x_loaded = read_and_decode_single_example(['../../test.NEWNEWNEW']);#'../../denseData_2017-11-06_08-57-41.NEWNEWNEW', '../../denseData_2017-11-06_08-57-41.NEWNEWNEW'])
+# print x_loaded
+# with tf.Session() as sess:
+#     # ... init our variables, ...
+#     sess.run(tf.global_variables_initializer())
+#     tf.train.start_queue_runners(sess=sess)
+#     x_input = sess.run([x_loaded])
+#     print(x_input)
