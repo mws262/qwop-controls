faaff71e1ba960bafb901def89a2d4110054b963
Matt Sheen <mws262@cornell.edu>
2018-01-15 17:37:15 -0500

    Setting up a net with the new tfrecord files
    Former-commit-id: 8d0762c24006a6f55d4e13193165e3816540efdb
    Former-commit-id: 66221dd9536d1efd723fe64b73712365a08bf90e
    Former-commit-id: e39b58aa312134170fbb0977e1b8683b2e690c87

--- src/python/Test_Java_TFRecord.py
+++ src/python/Test_Java_TFRecord.py
@@ -28,16 +28,18 @@ def read_and_decode_single_example(filename):
         sequence_features={
             # We know the length of both fields. If not the
             # tf.VarLenFeature could be used
+            'BODY': tf.FixedLenSequenceFeature([6],tf.float32,True),
             'PRESSED_KEYS': tf.FixedLenSequenceFeature([],tf.string,True),
             'TIME_TO_TRANSITION': tf.FixedLenSequenceFeature([],tf.string,True),
             'ACTIONS': tf.FixedLenSequenceFeature([],tf.string,True)
         })
 
     # now return the converted data
+    body = features[1]['BODY']
     pk = tf.decode_raw(features[1]['PRESSED_KEYS'],tf.uint8)
     tt = tf.decode_raw(features[1]['TIME_TO_TRANSITION'],tf.uint8),
     act = tf.decode_raw(features[1]['ACTIONS'],tf.uint8)
-    return pk,tt,act
+    return pk,tt,act,body
 
 filename_list = []
 print os.listdir('../../')
@@ -53,7 +55,7 @@ print('%d files in queue.' % len(filename_list))
 options = tf.python_io.TFRecordOptions(tf.python_io.TFRecordCompressionType.NONE)
 
 
-pk,tt,act = read_and_decode_single_example(['../../denseDataTest.NEWNEWNEW']);#'../../denseData_2017-11-06_08-57-41.NEWNEWNEW', '../../denseData_2017-11-06_08-57-41.NEWNEWNEW'])
+pk,tt,act,body = read_and_decode_single_example(['../../denseDataTest.NEWNEWNEW'])#'../../denseData_2017-11-06_08-57-41.NEWNEWNEW', '../../denseData_2017-11-06_08-57-41.NEWNEWNEW'])
 with tf.Session() as sess:
     # ... init our variables, ...
     sess.run(tf.global_variables_initializer())
@@ -61,6 +63,12 @@ with tf.Session() as sess:
     pkIn = sess.run([pk])
     ttIn = sess.run([tt])
     actIn = sess.run([act])
+    bodyIn = sess.run([body])
     print pkIn
     print ttIn
     print actIn
+    print bodyIn
+
+    for i in range(10):
+        ttIn = sess.run([tt])
+        print ttIn
