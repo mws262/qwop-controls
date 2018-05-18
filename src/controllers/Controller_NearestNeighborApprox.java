70146f306952634beffdd46e8c6208af0a5ab226
Matt <mws262@cornell.edu>
2018-05-18 08:00:34 -0400

    Lots2
    Former-commit-id: 11973e7cf78da8f98bb53acfefcb072a6d71dc25

--- src/controllers/Controller_NearestNeighborApprox.java
+++ src/controllers/Controller_NearestNeighborApprox.java
@@ -39,15 +39,27 @@ import main.Utility;
 
 
 public class Controller_NearestNeighborApprox implements IController {
-	// TODO: "Snapping" to certain sequences.
-	// TODO: Preferential treatment of sequences with more than a few timesteps remaining.
-	// TODO: Recent history matching.
+	/** Selection options! **/
+	State.ObjectName sortByPart = State.ObjectName.BODY;
+	State.StateName sortBySt = State.StateName.TH;
 
+	public boolean penalizeEndOfSequences = true;
+	public float maxPenaltyForEndOfSequence = 15f; // Penalty towards choosing runs near the end of their sequences.
+
+	public boolean comparePreviousStates = true;
+	public int numPreviousStatesToCompare = 5;
+	public float previousStatePenaltyMult = 1f;
+
+	public boolean enableVoting = false;
+	public int numTopMatchesToConsider = 10;
+	
+	public boolean enableTrajectorySnapping = false;
+	public float trajectorySnappingThreshold = 5f;
 
 	/** How many nearby (in terms of body theta) states are compared when determining the "closest" one. **/
 	public int upperSetLimit = 10000;
 	public int lowerSetLimit = 10000;
-	
+
 	/** Total number of states loaded from TFRecord file. **/
 	private int numStatesLoaded = 0;
 
@@ -59,14 +71,13 @@ public class Controller_NearestNeighborApprox implements IController {
 
 	/** Keep track of which total run that the currently selected action comes from. **/
 	private RunHolder currentTrajectory;
-	private StateHolder currentState;
-	private Deque<State> previousStatesLIFO = new LIFOFixedSize<State>(5);
+	private StateHolder currentTrajectoryStateMatch;
+	private Deque<State> previousStatesLIFO = new LIFOFixedSize<State>(numPreviousStatesToCompare);
 
-	State.ObjectName sortByPart = State.ObjectName.BODY;
-	State.StateName sortBySt = State.StateName.TH;
+	EvictingTreeMap<Float, StateHolder> topMatches = new EvictingTreeMap<Float, StateHolder>(numTopMatchesToConsider);
+
+	private boolean[] chosenKeys = new boolean[4];
 	
-	EvictingTreeMap<Float, StateHolder> topMatches = new EvictingTreeMap<Float, StateHolder>(10);
-
 	public Controller_NearestNeighborApprox(List<File> files) {
 		loadAll(files);
 		System.out.println("Wow! " + numStatesLoaded + " states were loaded!");
@@ -74,172 +85,93 @@ public class Controller_NearestNeighborApprox implements IController {
 
 	@Override
 	public Action policy(State state) {
-		//Utility.tic();
 		// Get nearest states (determined ONLY by body theta).
 		float sortBy = state.getStateVarFromName(sortByPart, sortBySt);
-		
+
 		NavigableMap<Float, StateHolder> lowerSet = allStates.headMap(sortBy, true);
 		NavigableMap<Float, StateHolder> upperSet = allStates.tailMap(sortBy, false);	
-		
-//		Iterator<StateHolder> iterLower = lowerSet.values().iterator();
-//		Iterator<StateHolder> iterUpper = upperSet.values().iterator();
 
-		StateHolder bestSoFar = null;
-		float bestErrorSoFar = Float.MAX_VALUE;
+		StateHolder bestMatch = null;
+		float bestMatchError = Float.MAX_VALUE;
 
-//		//		// Check full sq error with nearest bunch of states with lower body theta
-//				Utility.tic();
-//				int counter = 0;
-//				while (iterLower.hasNext() && counter++ < maxHalfExtent) {
-//					StateHolder st = iterLower.next();
-//					float error = totalEvalFunction(st, state);
-//					if (error < bestErrorSoFar) {
-//						bestSoFar = st;
-//						bestErrorSoFar = error;
-//					}
-//				}
-//				//System.out.print(bestErrorSoFar + ", ");
-//				//System.out.println(counter + " compared from below.");
-//		
-//				// Check full sq error with nearest bunch of states with higher body theta
-//				
-//				counter = 0;
-//				while (iterUpper.hasNext() && counter++ < maxHalfExtent) {
-//					StateHolder st = iterUpper.next();
-//					float error = totalEvalFunction(st, state);
-//					if (error < bestErrorSoFar) {
-//						bestSoFar = st;
-//						bestErrorSoFar = error;
-//					}
-//				}
-//				System.out.print("old ");
-//
-//				Utility.toc();
-		
-		
-//		Utility.tic();
-//		StateHolder sh1 = lowerSet.values().parallelStream().limit(lowerSetLimit).min(Comparator.comparingDouble( v -> totalEvalFunction(v, state))).orElse(null);
-//		StateHolder sh2 = upperSet.values().parallelStream().limit(upperSetLimit).min(Comparator.comparingDouble( v -> totalEvalFunction(v, state))).orElse(null);
-//		Utility.toc();
-		
 		EvictingTreeMap<Float, StateHolder> topMatches = new EvictingTreeMap<Float, StateHolder>(10);
-		
-		Utility.tic();
+
+		//Utility.tic();
 		lowerSet.values().parallelStream().limit(lowerSetLimit).forEach(v -> topMatches.put(totalEvalFunction(v, state),v));
 		upperSet.values().parallelStream().limit(upperSetLimit).forEach(v -> topMatches.put(totalEvalFunction(v, state),v));
-		Utility.toc();
-		
-		Iterator<Float> iter = topMatches.keySet().iterator();
-		
-		float keySum = 0;
-		float[] keysWeighted = new float[4]; 
-		while (iter.hasNext()) {
-			Float k = iter.next();
-			StateHolder v = topMatches.get(k);
-			
-			float multiplier = 1f/Float.max(k, Float.MIN_VALUE);
-			keySum += multiplier;
-			keysWeighted[0] += multiplier * (v.keys[0] ? 1 : 0);
-			keysWeighted[1] += multiplier * (v.keys[1] ? 1 : 0);
-			keysWeighted[2] += multiplier * (v.keys[2] ? 1 : 0);
-			keysWeighted[3] += multiplier * (v.keys[3] ? 1 : 0);
-			
-			
-			
-			System.out.println(
-			k + ", \t"
-			+ v.keys[0] + "," + v.keys[1] + "," + v.keys[2] + "," + v.keys[3]);
-		}
-		
-		keysWeighted[0] /= keySum;
-		keysWeighted[1] /= keySum;
-		keysWeighted[2] /= keySum;
-		keysWeighted[3] /= keySum;
-		
-//		System.out.println(keysWeighted[0] + "," + keysWeighted[1] + "," + keysWeighted[2] + "," + keysWeighted[3]);
-		Entry<Float, StateHolder> bestEntry = topMatches.pollFirstEntry();
-		bestSoFar = bestEntry.getValue();
-		bestErrorSoFar = bestEntry.getKey();
-		System.out.println(state.body.x);
-		
-		boolean[] chosenKeys = new boolean[4];
-		chosenKeys[0] = keysWeighted[0] >= 0.5f;
-		chosenKeys[1] = keysWeighted[1] >= 0.5f;
-		chosenKeys[2] = keysWeighted[2] >= 0.5f;
-		chosenKeys[3] = keysWeighted[3] >= 0.5f;
-		
-		if (!Arrays.equals(chosenKeys, bestEntry.getValue().keys)) {
-			System.out.println("WOW, OVERRIDING THE WINNER");
-		}
-//		Utility.tic();
-//		List<StateHolder> sh3 = lowerSet.values().parallelStream()
-//				.limit(upperSetLimit)
-//				.sorted(Comparator.comparingDouble( v -> totalEvalFunction(v, state)))
-//				.limit(5)
-//				.collect(Collectors.toList());
-//		List<StateHolder> sh4 = upperSet.values().parallelStream()
-//				.limit(upperSetLimit)
-//				.sorted(Comparator.comparingDouble( v -> totalEvalFunction(v, state)))
-//				.limit(5)
-//				.collect(Collectors.toList());
-//		Utility.toc();
-//		
-//		System.out.println("");
-//		for (StateHolder sh : sh3) {
-//			System.out.println(
-//					totalEvalFunction(sh, state) + ", \t"
-//					+ sh.keys[0] + "," + sh.keys[1] + "," + sh.keys[2] + "," + sh.keys[3]);
-//		}
-//		for (StateHolder sh : sh4) {
-//			System.out.println(
-//					totalEvalFunction(sh, state) + ", \t"
-//					+ sh.keys[0] + "," + sh.keys[1] + "," + sh.keys[2] + "," + sh.keys[3]);
-//		}
-		
-//		float lowSetErr = (sh1 == null) ? Float.MAX_VALUE : totalEvalFunction(sh1, state);
-//		float upperSetErr = (sh2 == null) ? Float.MAX_VALUE : totalEvalFunction(sh2, state);
-//		
-//		if (upperSetErr > lowSetErr) {
-//			bestSoFar = sh1;
-//			bestErrorSoFar = lowSetErr;
-//		}else {
-//			bestSoFar = sh2;
-//			bestErrorSoFar = upperSetErr;
-//		}
-		//System.out.print(state.body.x + ", \t");
-
-
 		//Utility.toc();
+		Entry<Float, StateHolder> bestEntry = topMatches.firstEntry();
+		bestMatch = bestEntry.getValue();
+		bestMatchError = bestEntry.getKey();
+		
+		
+		// Voting?
+		if (enableVoting) {
+			Iterator<Float> iter = topMatches.keySet().iterator();
+			float keySum = 0;
+			float[] keysWeighted = new float[4]; 
+			while (iter.hasNext()) {
+				Float k = iter.next();
+				StateHolder v = topMatches.get(k);
+
+				float multiplier = 1f/Float.max(k, Float.MIN_VALUE);
+				keySum += multiplier;
+				keysWeighted[0] += multiplier * (v.keys[0] ? 1 : 0);
+				keysWeighted[1] += multiplier * (v.keys[1] ? 1 : 0);
+				keysWeighted[2] += multiplier * (v.keys[2] ? 1 : 0);
+				keysWeighted[3] += multiplier * (v.keys[3] ? 1 : 0);
+
+				System.out.println(
+						k + ", \t"
+								+ v.keys[0] + "," + v.keys[1] + "," + v.keys[2] + "," + v.keys[3]);
+			}
+
+			keysWeighted[0] /= keySum;
+			keysWeighted[1] /= keySum;
+			keysWeighted[2] /= keySum;
+			keysWeighted[3] /= keySum;
+
+			chosenKeys[0] = keysWeighted[0] >= 0.5f;
+			chosenKeys[1] = keysWeighted[1] >= 0.5f;
+			chosenKeys[2] = keysWeighted[2] >= 0.5f;
+			chosenKeys[3] = keysWeighted[3] >= 0.5f;
+
+			if (!Arrays.equals(chosenKeys, bestEntry.getValue().keys)) {
+				System.out.println("Top match overridden by voting.");
+			}		
+		}else {
+			chosenKeys = bestMatch.keys;
+		}
 
 		// Choose whether to stay on the previous trajectory or not.
-//		if (currentTrajectory != null) {
-//			int lastStIdx = currentTrajectory.states.indexOf(currentState);
-//
-//			if (lastStIdx < currentTrajectory.states.size() - 2) {
-//
-//				StateHolder nextStateOnOldTraj = currentTrajectory.states.get(lastStIdx + 1);
-//				float oldTrajError = sqError(nextStateOnOldTraj.state, state);
-//
-//				if (bestErrorSoFar + 5 < oldTrajError) {
-//					currentTrajectory = bestSoFar.parentRun;
-//					currentState = bestSoFar;
-//				}else {
-//					//System.out.print("SNAP");
-//					currentState = nextStateOnOldTraj;
-//				}
-//			}else {
-//				currentTrajectory = bestSoFar.parentRun;
-//				currentState = bestSoFar;
-//			}
-//		}else {
-//
-			currentTrajectory = bestSoFar.parentRun;
-			currentState = bestSoFar;
-//		}
+		if (enableTrajectorySnapping && currentTrajectory != null) {
+			int lastStIdx = currentTrajectory.states.indexOf(currentTrajectoryStateMatch);
 
+			if (lastStIdx < currentTrajectory.states.size() - 2) {
+
+				StateHolder nextStateOnOldTraj = currentTrajectory.states.get(lastStIdx + 1);
+				float oldTrajError = sqError(nextStateOnOldTraj.state, state);
+
+				if (bestMatchError + trajectorySnappingThreshold < oldTrajError) {
+					currentTrajectory = bestMatch.parentRun;
+					currentTrajectoryStateMatch = bestMatch;
+				}else {
+					//System.out.print("SNAP");
+					currentTrajectoryStateMatch = nextStateOnOldTraj;
+					chosenKeys = currentTrajectoryStateMatch.keys;
+				}
+			}else {
+				currentTrajectory = bestMatch.parentRun;
+				currentTrajectoryStateMatch = bestMatch;
+			}
+		}else {
+			currentTrajectory = bestMatch.parentRun;
+			currentTrajectoryStateMatch = bestMatch;
+		}
+		
 		previousStatesLIFO.push(state); // Keep previous states too.
 
-		return new Action(1, bestSoFar.keys);
+		return new Action(1, chosenKeys);
 	}
 
 
@@ -247,34 +179,37 @@ public class Controller_NearestNeighborApprox implements IController {
 		float cost = 0f;
 
 		// Error relative to current state.
-
 		float currentStateError = sqError(sh.state, actualState);
 		cost += currentStateError;
 
-		// Sequences near their ends get a penalty.
 		int parentRunLength = sh.parentRun.states.size();
 		int stateLocInSequence = sh.parentRun.states.indexOf(sh);
 
-		float futureSequenceSizeError = 15f/(float)(parentRunLength - stateLocInSequence + 1);
-		cost += futureSequenceSizeError;
-
-		// 
-		int count = 1;
-		Iterator<State> iter = previousStatesLIFO.iterator();
-		float oldStateError = 0;
-		while (iter.hasNext()) {
-			State oldState = iter.next();
-			int idx = stateLocInSequence - count;
-			if (idx >= 0) {
-				State stateFromLibrary = sh.parentRun.states.get(idx).state;
-				oldStateError += (0.1f/(float)count)*sqError(oldState, stateFromLibrary);
-			}else {
-				oldStateError += 0.1f * currentStateError;
-			}
-			count++;
+		// Sequences near their ends get a penalty.
+		if (penalizeEndOfSequences) {
+			float futureSequenceSizeError = maxPenaltyForEndOfSequence/(float)(parentRunLength - stateLocInSequence + 1);
+			cost += futureSequenceSizeError;
 		}
-		//System.out.println("currentStateError: " + currentStateError + ", oldStateError: " + oldStateError + ", futureError: " + futureSequenceSizeError);
-		cost += oldStateError;
+
+		// Also compare previous states.
+		if (comparePreviousStates) {
+			int count = 1;
+			Iterator<State> iter = previousStatesLIFO.iterator();
+			float oldStateError = 0;
+			while (iter.hasNext()) {
+				State oldState = iter.next();
+				int idx = stateLocInSequence - count;
+				if (idx >= 0) {
+					State stateFromLibrary = sh.parentRun.states.get(idx).state;
+					oldStateError += (previousStatePenaltyMult/(float)count)*sqError(oldState, stateFromLibrary);
+				}else {
+					oldStateError += previousStatePenaltyMult * currentStateError;
+				}
+				count++;
+			}
+			//System.out.println("currentStateError: " + currentStateError + ", oldStateError: " + oldStateError + ", futureError: " + futureSequenceSizeError);
+			cost += oldStateError;
+		}
 		return cost;
 	}
 
@@ -392,9 +327,10 @@ public class Controller_NearestNeighborApprox implements IController {
 
 	@Override
 	public void draw(Graphics g, GameLoader game, float runnerScaling, int xOffsetPixels, int yOffsetPixels) {
-		if (currentTrajectory != null && currentState != null) {
-			int startIdx = currentTrajectory.states.indexOf(currentState);
-			float bodyX = currentState.state.body.x;
+		if (!previousStatesLIFO.isEmpty()) g.drawString(String.valueOf(previousStatesLIFO.peek().body.x), 500, 200);
+		if (currentTrajectory != null && currentTrajectoryStateMatch != null) {
+			int startIdx = currentTrajectory.states.indexOf(currentTrajectoryStateMatch);
+			float bodyX = currentTrajectoryStateMatch.state.body.x;
 			for (int i = 0; i < currentTrajectory.states.size(); i++) {
 				if (i % 20 == 0) {
 					game.drawExtraRunner((Graphics2D)g, currentTrajectory.states.get(i).state, "", runnerScaling, xOffsetPixels - (int)(runnerScaling * bodyX), yOffsetPixels, PanelRunner.ghostGray, PanelRunner.normalStroke);
