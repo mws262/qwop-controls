ec350d5060894e2b9aea1e99c49779d24cd11b31
Matt Sheen <mws262@cornell.edu>
2018-10-19 21:06:53 -0400

    Consolidated some file manipulation methods into one class

--- src/main/java/data/SavableFileIO.java
+++ src/main/java/data/SavableFileIO.java
@@ -1,10 +1,10 @@
 package data;
 
 import java.io.*;
-import java.util.Collection;
+import java.util.*;
 
 /**
- * Store and load serialized objects.
+ * Store and load serialized objects. Each of these savers can only handle saving and loading objects of one type.
  *
  * @param <T> Type of object manipulated.
  * @author matt
@@ -12,7 +12,7 @@ import java.util.Collection;
 public class SavableFileIO<T> {
 
     /**
-     * Whether to display debugging messages.
+     * Whether to display debugging/progress messages.
      */
     public boolean verbose = true;
 
@@ -33,7 +33,8 @@ public class SavableFileIO<T> {
             }
         }
 
-        if (!append || !saveFile.exists()) {
+        if (!append || !saveFile.exists()) { // If the file doesn't exist, or we just don't want to append, then
+            // don't use the appending version of the output stream.
             try (ObjectOutputStream objOps = new ObjectOutputStream(new FileOutputStream(saveFile, false))) {
                 dataToStream(data, objOps);
             } catch (IOException e) {
@@ -48,16 +49,13 @@ public class SavableFileIO<T> {
         }
     }
 
-    private void dataToStream(Collection<T> data, ObjectOutputStream objOps) throws IOException {
-        int count = 0;
-        for (T d : data) {
-            objOps.writeObject(d);
-            count++;
-            if (verbose) System.out.println("Wrote games to file: " + count + "/" + data.size());
-        }
-        objOps.flush();
-    }
-
+    /**
+     * Load objects from a save file. They will be put in the provided collection. The provided collection will not
+     * be cleared before adding objects.
+     *
+     * @param file Saved file to load from.
+     * @param collection Collection (e.g. {@link java.util.List}, {@link java.util.Set}) to put the loaded objects in.
+     */
     public void loadObjectsToCollection(File file, Collection<T> collection) {
         int counter = 0;
         try (ObjectInputStream objIs = new ObjectInputStream(new FileInputStream(file))) {
@@ -81,6 +79,74 @@ public class SavableFileIO<T> {
             e.printStackTrace();
         }
         if (verbose) System.out.println("Loaded " + counter + " objects from file " + file.getName() + ".");
+    }
+
+    /**
+     * Send objects to an output stream.
+     *
+     * @param data Collection of objects to send to output stream.
+     * @param objOps Output stream. Could be the usual version or the appending version.
+     * @throws IOException When writing to stream fails.
+     */
+    private void dataToStream(Collection<T> data, ObjectOutputStream objOps) throws IOException {
+        int count = 0;
+        for (T d : data) {
+            objOps.writeObject(d);
+            count++;
+            if (verbose) System.out.println("Wrote games to file: " + count + "/" + data.size());
+        }
+        objOps.flush();
+    }
+
+    /**
+     * Combine multiple files into one output file, eliminating any duplicates. Does not delete the original files.
+     *
+     * @param inputFiles Multiple files with saved objects that we wish to combine into a single file. All these
+     *                   files must contain the same object type.
+     * @param destination Output file for the combined objects found in inputFiles.
+     */
+    public void combineFiles(File[] inputFiles, File destination) {
+        HashSet<T> loadedSet = new HashSet<>();
+        // Load them all into the same set.
+        for (File file : inputFiles) {
+            loadObjectsToCollection(file, loadedSet);
+        }
+        storeObjects(loadedSet, destination, false);
+    }
+
+    /**
+     * Get all files in the working directory with a specified file extension.
+     * @param extension File extension. Do not include the dot before the extension.
+     * @return List of files with the specified file extension.
+     **/
+    public static Set<File> getFilesByExtension(File directory, String extension) {
+        File[] filesInDirectory = directory.listFiles();
+        Objects.requireNonNull(filesInDirectory, "Unable to open the current directory.");
+        Set<File> files = new HashSet<>();
+
+        for (File file : filesInDirectory) {
+            if (file.isFile()) {
+                int indexOfLastSeparator = file.getName().lastIndexOf(".");
+                // Only get the files with the specified file extension.
+                if (file.getName().substring(indexOfLastSeparator).equalsIgnoreCase(extension)) {
+                    files.add(file);
+                }
+            }
+        }
+        return files;
+    }
+
+    /**
+     * Print the size of a file in megabytes. For debugging stuff.
+     * @param file File to check the size of.
+     */
+    public static void printFileSize(File file) {
+        if (file.exists()) {
+            double bytes = file.length();
+            double kilobytes = (bytes / 1024);
+            double megabytes = (kilobytes / 1024);
+            System.out.println(file + " is: " + Math.round(megabytes * 100) / 100. + "Mb");
+        }
     }
 
     /**
