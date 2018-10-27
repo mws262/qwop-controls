02b35897cc56c7d996891f232ef6649cc66877df
Matt Sheen <mws262@cornell.edu>
2018-10-26 21:26:46 -0400

    Working on cold-start vs. warm-start examples. Basic writeup of the problem and the approach as a readme for that package.

--- src/main/java/goals/ColdStartAnalysis/MAIN_CompareWarmStartToColdMulti.java
+++ src/main/java/goals/ColdStartAnalysis/MAIN_CompareWarmStartToColdMulti.java
@@ -1,13 +1,33 @@
-package goals;
+package goals.ColdStartAnalysis;
 
 import actions.Action;
 import actions.ActionQueue;
 import game.GameLoader;
-import game.State;
+import tree.Node;
+import ui.PanelRunner_MultiState;
 
-public class MAIN_CompareWarmStartToCold {
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Take a known sequence of reasonable actions, and introduce extra runners all along the way with the same state,
+ * but lacking the warm-start of the physics engine solver. Simulate these all with the same commands at each timestep.
+ * @author matt
+ */
+public class MAIN_CompareWarmStartToColdMulti extends JFrame {
+
+    /**
+     * Every how many actions we introduce a new cold-started runner to simulate and visualize.
+     */
+    private int coldStartRunnerIntroductionInterval = 4;
 
     public static void main(String[] args) {
+        new MAIN_CompareWarmStartToColdMulti().run();
+    }
+    public void run() {
+        // Ran MAIN_Search_LongRun to get these.
         ActionQueue actionQueue = new ActionQueue();
         actionQueue.addAction(new Action(5, new boolean[]{false, false, false, false}));
         actionQueue.addAction(new Action(38, new boolean[]{false, true, true, false}));
@@ -58,26 +78,48 @@ public class MAIN_CompareWarmStartToCold {
         actionQueue.addAction(new Action(46, new boolean[]{false, true, true, false}));
         actionQueue.addAction(new Action(17, new boolean[]{false, false, false, false}));
 
+        // Vis setup.
+        PanelRunner_MultiState panel = new PanelRunner_MultiState();
+        panel.activateTab();
+        getContentPane().add(panel);
+        setPreferredSize(new Dimension(1000, 400));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setVisible(true);
+
         GameLoader gameFullRun = new GameLoader(); // This game will run all the commands, start to finish.
-        GameLoader gameColdStart = new GameLoader(); // This will start at some point in the middle of the sequence,
-        // with a cloned state from gameFullRun, but a cold start on all the internal solvers.
+        List<GameLoader> coldStartGames = new ArrayList<>();
+        int coldStartAction = 2; // Starts the first cold start runner at this command index.
 
-        int coldStartAction = 12;
+        while(!actionQueue.isEmpty()){ // Go until all commands used.
+            // Every action interval, introduce a new cold-start runner and resume simulating all of them.
+            while (actionQueue.getCurrentActionIdx() < coldStartAction && !actionQueue.isEmpty()) {
+                panel.clearSecondaryStates(); // Clear all the visualized runners.
 
-        while (actionQueue.getCurrentActionIdx() < 12) {
-            gameFullRun.stepGame(actionQueue.pollCommand());
+                boolean[] nextCommand = actionQueue.pollCommand(); // Next command.
+                gameFullRun.stepGame(nextCommand); // Sim the main runner and put on screen.
+                panel.setMainState(gameFullRun.getCurrentState());
+
+                // Simulate the additional cold-started runners.
+                for (int i = 0; i < coldStartGames.size(); i++) {
+                    coldStartGames.get(i).stepGame(nextCommand);
+                        panel.addSecondaryState(coldStartGames.get(i).getCurrentState(),
+                                Node.getColorFromScaledValue(i, actionQueue.getActionsInCurrentRun().length/5, 0.8f));
+                }
+                repaint();
+                try {
+                    Thread.sleep(40);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            // Add a new cold started runner at the full game's current state.
+            GameLoader coldStartGame = new GameLoader();
+            coldStartGame.setState(gameFullRun.getCurrentState());
+            coldStartGames.add(coldStartGame);
+
+            coldStartAction += coldStartRunnerIntroductionInterval;
         }
-        State coldStartState = gameFullRun.getCurrentState();
-
-        gameColdStart.setState(coldStartState);
-
-        float[] assignedState = gameColdStart.getCurrentState().flattenState();
-        float[] originalState = gameFullRun.getCurrentState().flattenState();
-
-        // Check.
-        for (int i = 0; i < assignedState.length; i++) {
-            System.out.println( assignedState[i] == originalState[i]);
-        }
-
     }
 }
---
+++ src/main/java/goals/ColdStartAnalysis/README.md
@@ -0,0 +1,55 @@
+##How much does the warm-start matter?
+####Background
+QWOP's physics engine, Box2D (or [JBox2D](http://www.jbox2d.org/) for my Java version), enforces all constraints with
+ impulses. At every timestep, these impulses apply gravity, keep the joints from drifting apart, and keep the feet from 
+ sinking through the ground. Box2D has to solve for impulses which simultaneously satisfy all these constraints.
+
+As is true for most games, a reliably fast solution is more important than a super-accurate one (see 
+[*anytime 
+algorithms*](https://en.wikipedia.org/wiki/Anytime_algorithm)). QWOP's physics 
+are 
+stepped forward in time with the game's visual updates. We don't want the game's framerate to be slow or have 
+occasional spikes. 
+
+Box2D uses an iterative solver, [Projected Gauss-Seidel](https://en.wikipedia.org/wiki/Gauss%E2%80%93Seidel_method), 
+to find all the impulses which approximately satisfy physical constraints. QWOP, in particular, only runs a few 
+iterations off this solver (Recall that QWOP needed to run in realtime in 2008 Internet Explorer.). The result isn't 
+as bad as it might sound. In between two timesteps, the active constraints probably don't change much. The solution 
+from the previous timestep can be a great starting point for solving the constraints at the current timestep. This 
+use of a previous solution is called *warm-starting* the solver (as opposed to *cold-starting*). Over many timesteps, 
+the solver gets close to the 
+true solution, and chases it around pretty well, as long as the constraints don't change too rapidly.
+
+Since QWOP's constraint solver only runs a few iterations, its result is *highly dependent* on the solution at the 
+previous timestep, which is highly dependent on the timestep before that. And so on. In a sense, these solver 
+solutions act like state variables, evolving over time with their own dynamics. These additional "states" are 
+particularly annoying since they can't be directly observed on the screen. We could potentially infer these hidden 
+solver states from the entire history of previous observable states and actions, but we don't know how the 
+hidden states behave, at least not without modelling aspects of Box2D that I don't care to. We also can't set these 
+states without hacking Box2D more than I care to.
+
+As a result, if two QWOP runners look like they are in the same state and we apply the same action to both, they will
+ only have the same final state if both runners have the same state/action history. When searching for 
+ actions, we cannot just arbitrarily set the runner state and assume that the actions we find to locomote will 
+ generalize to all ways that the runner ended up in that (observable) state. However, the problem cannot be hopeless.
+  Humans can play QWOP (albeit, not very well). It seems reasonable that people might use a small amount of state 
+  history to decide when to hit buttons, but it's hard to believe that people have 10 or 20 previous steps memorized.
+  
+  So, we know that the internal physics engine states have an effect, which can be thought of as a state history 
+  dependence. Here, we are trying to figure out how large this effect is, or perhaps, how long is the time horizon of
+   this history dependence. Can we completely neglect this effect, or is it vital that we pay attention to the 
+   state/action history or try to estimate the hidden state?
+ 
+#### Approaches
+1. Find a "good" run using one of the tree searches.
+2. Simulate the good run's actions partway.
+3. Introduce a new runner at the other's observable state, but with a cold start for the constraint solver. 
+4. Continue to simulate both runners with the same actions.
+5. See how long, and it what way, the cold-start runner diverges.
+6. Investigate when the sensitivity is highest/lowest. When, throughout the gait, are the warm-start's effects most 
+significant?
+
+#### Results
+TBD. Seems like divergence happens really quickly, maybe 
+1.5 - 3 steps.
+
