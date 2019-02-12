73c07d03aede895bc679d79cb490b18f93f41cea
matt <mws262@cornell.edu>
2019-02-12 18:37:01 -0500

    meh

--- src/main/java/evaluators/EvaluationFunction_Velocity.java
+++ src/main/java/evaluators/EvaluationFunction_Velocity.java
@@ -3,21 +3,18 @@ package evaluators;
 import tree.Node;
 
 /**
- * Evaluation of a node based distance traveled since the last node.
+ * Evaluation of a node based strictly on torso horizontal position.
  *
  * @author Matt
  */
-public class EvaluationFunction_DeltaDistance implements IEvaluationFunction {
+public class EvaluationFunction_Velocity implements IEvaluationFunction {
 
     @Override
     public float getValue(Node nodeToEvaluate) {
         if (nodeToEvaluate.isStateUnassigned())
             throw new NullPointerException("Trying to evaluate a node based on state information which has not yet " +
                     "been assigned in that node.");
-        if (nodeToEvaluate.getTreeDepth() > 0)
-            return nodeToEvaluate.getState().body.getX() - nodeToEvaluate.getParent().getState().body.getX();
-        else
-            return 0;
+        return nodeToEvaluate.getState().body.getDx();
     }
 
     @Override
@@ -26,7 +23,7 @@ public class EvaluationFunction_DeltaDistance implements IEvaluationFunction {
     }
 
     @Override
-    public EvaluationFunction_DeltaDistance getCopy() {
-        return new EvaluationFunction_DeltaDistance();
+    public EvaluationFunction_Velocity getCopy() {
+        return new EvaluationFunction_Velocity();
     }
 }
