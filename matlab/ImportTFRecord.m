e96f35dbc3b19e3e0ff2655a14b48e90b61288ab
matt <mws262@cornell.edu>
2019-02-21 17:55:26 -0500

    more changes to value fcn

--- matlab/ImportTFRecord.m
+++ matlab/ImportTFRecord.m
@@ -19,10 +19,63 @@ for k = 1:length(files)
     fprintf('loaded %d runs from file.\n', loadedFileData.size());
     
     for i = 0:loadedFileData.size() - 1
-        loadedStates = [loadedStates, dataParser.getStatesFromLoadedSequence(loadedFileData.get(i))];
-        loadedCommands = [loadedCommands; dataParser.getCommandSequenceFromLoadedSequence(loadedFileData.get(i))];
+        loadedStates = dataParser.getStatesFromLoadedSequence(loadedFileData.get(i));
+        loadedCommands = dataParser.getCommandSequenceFromLoadedSequence(loadedFileData.get(i));
+        
+        bodyY = zeros(1, length(loadedStates));
+        bodyTh = zeros(1, length(loadedStates));
+        count = 1;
+        for j = 1:length(loadedStates)
+            %     if isempty(loadedStates(i))
+            %         continue;
+            %     end
+            bodyY(count) = loadedStates(j).body.getY();
+            bodyTh(count) = loadedStates(j).body.getTh();
+            count = count + 1;
+        end
+        noneIdx = loadedCommands(:, 1) == 0 & loadedCommands(:, 2) == 0;
+        woIdx = loadedCommands(:, 2) == 1;
+        qpIdx = loadedCommands(:, 1) == 1;
+        subplot(3,1,1);
+        axis([-5,5,-5,5]);
+        plot(bodyY(:, noneIdx)', bodyTh(:, noneIdx)', '.r');
+        hold on;
+        subplot(3,1,2);
+        axis([-5,5,-5,5]);
+        
+        plot(bodyY(:, woIdx)', bodyTh(:, woIdx)', '.g');
+        hold on;
+        subplot(3,1,3);
+        axis([-5,5,-5,5]);
+        
+        plot(bodyY(:, qpIdx)', bodyTh(:, qpIdx)', '.b');
+        hold on;
+        drawnow;
     end
+    
 end
+
+% bodyY = zeros(1, length(loadedStates));
+% bodyTh = zeros(1, length(loadedStates));
+% count = 1;
+% for i = 1:length(loadedStates)
+%     %     if isempty(loadedStates(i))
+%     %         continue;
+%     %     end
+%     bodyY(count) = loadedStates(i).body.getY();
+%     bodyTh(count) = loadedStates(i).body.getTh();
+%     count = count + 1;
+% end
+%
+% noneIdx = loadedCommands(:, 1) == 0 & loadedCommands(:, 2) == 0;
+% woIdx = loadedCommands(:, 2) == 1;
+% qpIdx = loadedCommands(:, 1) == 1;
+% plot(bodyY(:, noneIdx)', bodyTh(:, noneIdx)', '.r');
+% hold on;
+% plot(bodyY(:, woIdx)', bodyTh(:, woIdx)', '.g');
+% plot(bodyY(:, qpIdx)', bodyTh(:, qpIdx)', '.b');
+
+
 % game = game.GameSingleThread;
 % [fig, ax, bodyPatches, headTForm] = DrawNewRunner(game.getDebugVertices);
 %
@@ -33,24 +86,4 @@ end
 %
 %     pause(0.001);
 % end
-
-bodyY = zeros(1, length(loadedStates));
-bodyTh = zeros(1, length(loadedStates));
-count = 1;
-for i = 1:length(loadedStates)
-    %     if isempty(loadedStates(i))
-    %         continue;
-    %     end
-    bodyY(count) = loadedStates(i).body.getY();
-    bodyTh(count) = loadedStates(i).body.getTh();
-    count = count + 1;
-end
-
-noneIdx = loadedCommands(:, 1) == 0 & loadedCommands(:, 2) == 0;
-woIdx = loadedCommands(:, 2) == 1;
-qpIdx = loadedCommands(:, 1) == 1;
-plot(bodyY(:, noneIdx)', bodyTh(:, noneIdx)', '.r');
-hold on;
-plot(bodyY(:, woIdx)', bodyTh(:, woIdx)', '.g');
-plot(bodyY(:, qpIdx)', bodyTh(:, qpIdx)', '.b');
 
--- src/main/java/goals/tree_search/MAIN_Search_ValueFun.java
+++ src/main/java/goals/tree_search/MAIN_Search_ValueFun.java
@@ -1,14 +1,19 @@
 package goals.tree_search;
 
+import actions.Action;
 import evaluators.EvaluationFunction_Constant;
+import evaluators.EvaluationFunction_DeltaDistance;
 import evaluators.EvaluationFunction_Distance;
+import game.GameThreadSafe;
 import samplers.Sampler_UCB;
+import samplers.rollout.RolloutPolicy_RandomColdStart;
 import samplers.rollout.RolloutPolicy_ValueFunction;
 import savers.DataSaver_StageSelected;
 import tflowtools.TrainableNetwork;
 import tree.Node;
 import tree.TreeStage_MaxDepth;
 import tree.TreeWorker;
+import tree.Utility;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -39,14 +44,37 @@ public class MAIN_Search_ValueFun extends MAIN_Search_Template {
 
         int numWorkersToUse = (int) Math.max(1, maxWorkerFraction * maxWorkers);
 
+        // Make new tree root and assign to GUI.
         // Assign default available actions.
         assignAllowableActions(-1);
-
-        // Make new tree root and assign to GUI.
         Node rootNode = new Node();
         Node.pointsToDraw.clear();
         ui.clearRootNodes();
         ui.addRootNode(rootNode);
+
+        List<Action[]> alist = new ArrayList<>();
+        alist.add(new Action[]{
+                new Action(1,false,false,false,false),
+                new Action(34,false,true,true,false),
+                new Action(19,false,false,false,false),
+                new Action(45,true,false,false,true),
+
+                new Action(10,false,false,false,false),
+                new Action(27,false,true,true,false),
+                new Action(8,false,false,false,false),
+                new Action(20,true,false,false,true),
+        });
+
+        Node.makeNodesFromActionSequences(alist, rootNode, new GameThreadSafe());
+        Node.stripUncheckedActionsExceptOnLeaves(rootNode, 7);
+
+        List<Node> leaf = new ArrayList<>();
+        rootNode.getLeaves(leaf);
+        assert leaf.size() == 1;
+        leaf.get(0).resetSweepAngle();
+
+
+
 
         DataSaver_StageSelected saver = new DataSaver_StageSelected();
         saver.overrideFilename = "tmp";
@@ -56,7 +84,7 @@ public class MAIN_Search_ValueFun extends MAIN_Search_Template {
         ArrayList<Integer> layerSizes = new ArrayList<>();
         layerSizes.add(73);
         layerSizes.add(128);
-        layerSizes.add(50);
+//        layerSizes.add();
         layerSizes.add(1);
 //        layerSizes.add(totalActionCount);
 //        List<String> extraNetworkArgs = new ArrayList<>();
@@ -69,12 +97,14 @@ public class MAIN_Search_ValueFun extends MAIN_Search_Template {
         valueNetwork.loadCheckpoint("chk1");
 
         for (int k = 0; k < 1000; k++) {
-            RolloutPolicy_ValueFunction valueFunction  =
+            RolloutPolicy_ValueFunction rollout  =
                     new RolloutPolicy_ValueFunction(new EvaluationFunction_Distance(), valueNetwork);
 
-            valueFunction.maxRolloutTimesteps = 200;
-//            Sampler_UCB ucbSampler = new Sampler_UCB(new EvaluationFunction_Distance(), valueFunction);
-            Sampler_UCB ucbSampler = new Sampler_UCB(new EvaluationFunction_Constant(0f), valueFunction);
+            rollout.maxRolloutTimesteps = 200;
+//            RolloutPolicy_RandomColdStart rollout = new RolloutPolicy_RandomColdStart(new EvaluationFunction_DeltaDistance());
+
+            //            Sampler_UCB ucbSampler = new Sampler_UCB(new EvaluationFunction_Distance(), valueFunction);
+            Sampler_UCB ucbSampler = new Sampler_UCB(new EvaluationFunction_Constant(0f), rollout);
 
 
             TreeStage_MaxDepth searchMax = new TreeStage_MaxDepth(getToSteadyDepth, ucbSampler, saver);
@@ -96,40 +126,88 @@ public class MAIN_Search_ValueFun extends MAIN_Search_Template {
             rootNode.getNodesBelow(nodesBelow, true);
             nodesBelow.remove(rootNode);
 
-            // Get states and actions for training step.
-            float[][] trainingStateArray = new float[nodesBelow.size()][73];
-//            float[][] actionDurations = new float[nodesBelow.size()][1];
-            float[][] value = new float[nodesBelow.size()][1];
+//            // Get states and actions for training step.
+//            float[][] trainingStateArray = new float[nodesBelow.size()][73];
+////            float[][] actionDurations = new float[nodesBelow.size()][1];
+//            float[][] value = new float[nodesBelow.size()][1];
+//
+//            for (int i = 0; i < nodesBelow.size(); i++) {
+//                float[] st = nodesBelow.get(i).getParent().getState().flattenState();
+//                for (int j = 0; j < st.length; j++) {
+//                    trainingStateArray[i][j] = st[j];
+//                }
+//                trainingStateArray[i][st.length] = nodesBelow.get(i).getAction().getTimestepsTotal();
+//                value[i][0] = nodesBelow.get(i).getValue()/nodesBelow.get(i).visitCount.floatValue();
+//
+//                // The fuck is going on here.
+//                if (Float.isNaN(value[i][0])) {
+//                    System.out.println("WAIT!");
+//                    System.out.println(nodesBelow.get(i).getValue());
+//                    System.out.println(nodesBelow.get(i).visitCount.floatValue());
+//                    try {
+//                        Thread.sleep(500);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                    System.out.println(nodesBelow.get(i).getValue());
+//                    System.out.println(nodesBelow.get(i).visitCount.floatValue());
+//                    value[i][0] = nodesBelow.get(i).getValue()/nodesBelow.get(i).visitCount.floatValue();
+//
+//                }
+////                actionDurations[i][nodesBelow.get(i).getAction().getTimestepsTotal() - actionLow] = 1; // One hot.
+//            }
+//
+//            float loss = valueNetwork.trainingStep(trainingStateArray, value, netTrainingStepsPerIter);
 
-            for (int i = 0; i < nodesBelow.size(); i++) {
-                float[] st = nodesBelow.get(i).getParent().getState().flattenState();
-                for (int j = 0; j < st.length; j++) {
-                    trainingStateArray[i][j] = st[j];
-                }
-                trainingStateArray[i][st.length] = nodesBelow.get(i).getAction().getTimestepsTotal();
-                value[i][0] = nodesBelow.get(i).getValue()/nodesBelow.get(i).visitCount.floatValue();
+            // Do as minibatches instead.
 
-                // The fuck is going on here.
-                if (Float.isNaN(value[i][0])) {
-                    System.out.println("WAIT!");
-                    System.out.println(nodesBelow.get(i).getValue());
-                    System.out.println(nodesBelow.get(i).visitCount.floatValue());
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+
+            int batchSize = 100;
+            int numBatches = nodesBelow.size()/batchSize;
+            float[][] trainingStateArray = new float[batchSize][73];
+            float[][] value = new float[batchSize][1];
+
+            int currentNodeIdx = 0;
+            for (int i = 0; i < numBatches; i++) {
+                List<Node> batchNodes = new ArrayList<>();
+                for (int j = 0; j < batchSize; j++) {
+                    Node currNode = nodesBelow.get(currentNodeIdx);
+                    batchNodes.add(currNode);
+                    float[] st = currNode.getParent().getState().flattenState();
+                    for (int m = 0; m < st.length; m++) {
+                        trainingStateArray[j][m] = st[m];
                     }
-                    System.out.println(nodesBelow.get(i).getValue());
-                    System.out.println(nodesBelow.get(i).visitCount.floatValue());
-                    value[i][0] = nodesBelow.get(i).getValue()/nodesBelow.get(i).visitCount.floatValue();
+                    trainingStateArray[j][st.length] = currNode.getAction().getTimestepsTotal();
+                    value[j][0] = currNode.getValue()/currNode.visitCount.floatValue();
 
+
+                    if (Float.isNaN(value[j][0])) {
+                        System.out.println("WAIT!");
+                        System.out.println(currNode.getValue());
+                        System.out.println(currNode.visitCount.floatValue());
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println(currNode.getValue());
+                        System.out.println(currNode.visitCount.floatValue());
+                        value[j][0] = currNode.getValue()/currNode.visitCount.floatValue();
+                    }
+                    currentNodeIdx++;
                 }
-//                actionDurations[i][nodesBelow.get(i).getAction().getTimestepsTotal() - actionLow] = 1; // One hot.
+                float loss = valueNetwork.trainingStep(trainingStateArray, value, netTrainingStepsPerIter);
+                float[][] result = valueNetwork.evaluateInput(trainingStateArray);
+
+                for (int n = 0; n < result.length; n++) {
+                    batchNodes.get(n).nodeLabelAlt = String.format("%.2f", result[n][0]);
+                }
+                System.out.println("Loss: " + loss + " Epoch: " + k + ", Batch: " + i);
             }
 
-            float loss = valueNetwork.trainingStep(trainingStateArray, value, netTrainingStepsPerIter);
+            System.out.println("Saved");
             valueNetwork.saveCheckpoint("chk1");
-            System.out.println("Training complete. Loss: " + loss + " Iteration: " + k);
+
 
         }
     }
