878075d9955d2b9730ffca46cde1912ac445695a
Matt <mws262@cornell.edu>
2019-04-13 23:19:02 -0400

    stuff for making some demo videos

--- flash/load_site.sh
+++ flash/load_site.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-google-chrome --new-window http://localhost:8000/index.html --incognito --window-size=680,550
+google-chrome --new-window http://localhost:8000/index.html --incognito --window-size=1440,1000
