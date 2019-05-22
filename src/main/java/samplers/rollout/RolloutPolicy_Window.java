21660cc1e9593976db96964ec29c0fd677586086
Matt <mws262@cornell.edu>
2019-05-22 08:57:37 -0400

    fixed test case

--- src/main/java/samplers/rollout/RolloutPolicy_Window.java
+++ src/main/java/samplers/rollout/RolloutPolicy_Window.java
@@ -27,25 +27,33 @@ public class RolloutPolicy_WorstCaseWindow extends RolloutPolicy {
         Action belowAction = new Action(middleAction.getTimestepsTotal() - 1, middleAction.peek());
 
 
-        // float startValue = evaluationFunction.getValue(startNode);
-        float valMid = individualRollout.rollout(startNode, game);// - startValue;
-        simGameToNode(startNode.getParent(), game);
+        float startValue = individualRollout.evaluationFunction.getValue(startNode);
+
+        float valMid = individualRollout.rollout(startNode, game);
+
+        if (startNode.getTreeDepth() > 1)
+            simGameToNode(startNode.getParent(), game);
         actionQueue.addAction(aboveAction);
         while (!actionQueue.isEmpty())
             game.step(actionQueue.pollCommand());
-        NodeQWOPExplorableBase<?> nodeAbove = startNode.getParent().addBackwardsLinkedChild(aboveAction, game.getCurrentState());
 
-        float valAbove = individualRollout.rollout(nodeAbove, game);// - startValue;
-        simGameToNode(startNode.getParent(), game);
+        NodeQWOPExplorableBase<?> nodeAbove = startNode.getParent().addBackwardsLinkedChild(aboveAction, game.getCurrentState());
+        float valAbove =
+                individualRollout.rollout(nodeAbove, game) + individualRollout.evaluationFunction.getValue(nodeAbove) - startValue;
+
+        if (startNode.getTreeDepth() > 1)
+            simGameToNode(startNode.getParent(), game);
         actionQueue.addAction(belowAction);
         while (!actionQueue.isEmpty())
             game.step(actionQueue.pollCommand());
+
         NodeQWOPExplorableBase<?> nodeBelow = startNode.getParent().addBackwardsLinkedChild(belowAction, game.getCurrentState());
 
 
-        float valBelow = individualRollout.rollout(nodeBelow, game);// - startValue;
+        float valBelow =
+                individualRollout.rollout(nodeBelow, game) + individualRollout.evaluationFunction.getValue(nodeBelow) - startValue;
 
-        return Math.min(valMid, Math.min(valAbove, valBelow)); // Gets the worst out of three.
+        return Math.max(valMid, Math.max(valAbove, valBelow)); // Gets the worst out of three.
     }
 
     @Override
