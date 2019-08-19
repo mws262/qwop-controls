90c3dbe1a648cb660caf32e1519693370c4a9ed5
Matt Sheen <mws262@cornell.edu>
2019-08-19 15:35:51 -0400

    early testing for dqn. cant get gym to work on laptop

---
+++ python/create_dqn_graph.py
@@ -0,0 +1,37 @@
+import create_generic_graph as tflow_utility
+import tensorflow as tf
+
+# TODO hardcoded sizes and parameters need to be arguments
+learning_rate = 1e-3
+
+## Define the network
+# State input
+input = tf.placeholder(tf.float32, shape=(None, 72), name='input')
+# Action target
+output_target = tf.placeholder(tf.float32, shape=(None, 9), name='output_target')  # Some values, same dim as the output layer, used in figuring out the loss.
+# Q target
+scalar_target = tf.placeholder(tf.float32, [None, ], name="scalar_target")  # Some scalar value tied up in figuring out the loss.
+
+# Fully-connected layers pre-processing the input states.
+pre_split_output = tflow_utility.sequential_layers(input, [72, 36, 10], "fully_connected", tf.nn.elu)
+
+# Net splits into two branches, one set of layers estimating the value, and the other estimating advantage.
+value = tflow_utility.sequential_layers(pre_split_output, [10, 5, 1], "value", tf.nn.elu)
+advantage = tflow_utility.sequential_layers(pre_split_output, [10, 5, 9], "advantage", tf.nn.elu)
+
+# Combine value and advantage into Q.
+output = value + tf.subtract(advantage, tf.reduce_mean(advantage, axis=1, keepdims=True))
+
+# Mask Q by the applicable action.
+Qscalar = tf.reduce_sum(tf.multiply(output, output_target), axis=1)
+
+# TODO PER weighting
+
+loss = tf.reduce_mean(tf.squared_difference(scalar_target, Qscalar))
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+
+init = tf.global_variables_initializer()
+saver_def = tf.train.Saver().as_saver_def()
+savepath = "../src/main/resources/tflow_models/dqn.pb"
+with open(savepath, 'wb') as f:
+    f.write(tf.get_default_graph().as_graph_def().SerializeToString())
--- python/create_generic_graph.py
+++ python/create_generic_graph.py
@@ -1,28 +1,7 @@
 import argparse
 import tensorflow as tf
 
-## Parse command line options defining the network.
-parser = argparse.ArgumentParser()
-parser.add_argument('-l', '--layers', nargs='+', type=int,
-                    help='<Required> Define fully-connected layer sizes from input to output', required=True)
-parser.add_argument('-s', '--savepath', type=str, help='<Required> File path and name to save the network graph to.',
-                    required=True)
-parser.add_argument('-lr', '--learnrate', type=float, help='<Optional> Optimizer learning rate (default = 1e-3).',
-                    required=False, default=1e-3)
-parser.add_argument('-w', '--weightstd', type=float,
-                    help='<Optional> Standard deviation initialization for weights. (default = 0.1).', required=False,
-                    default=0.1)
-parser.add_argument('-b', '--biasinit', type=float, help='<Optional> Initialization for biases. (default = 0.1).',
-                    required=False, default=0.1)
-parser.add_argument('-a', '--activations', type=str, help='<Optional> Nonlinear activation type (default leaky_relu).',
-                    required=False, default="leaky_relu", choices=['relu', 'leaky_relu', 'elu', 'sigmoid', 'tanh', 'identity'])
-parser.add_argument('-ao', '--activationsout', type=str, help='<Optional> Output layer activation.',
-                    required=False, default="identity", choices=['relu', 'leaky_relu', 'elu', 'sigmoid', 'tanh', 'identity', 'softmax'])
-parser.add_argument('-ls', '--loss', type=str, help='<Optional> Loss type.',
-                    required=False, default="huber", choices=['huber', 'mse', 'cross_entropy', 'policy_gradient', 'qlearn'])
 
-
-args = parser.parse_args()
 
 # Available activation functions to lookup.
 activation_options = {
@@ -34,17 +13,6 @@ activation_options = {
     "identity": tf.identity,
     "softmax": tf.nn.softmax
 }
-
-# Get user specified options (or defaults).
-layer_sizes = args.layers
-init_weight_stddev = args.weightstd
-init_bias_val = args.biasinit
-learning_rate = args.learnrate
-activations = activation_options.get(args.activations)
-output_activations = activation_options.get(args.activationsout)
-savepath = args.savepath
-loss_selection = args.loss
-
 
 def weight_variable(shape):
     """
@@ -89,7 +57,7 @@ def variable_summaries(var):
             tf.summary.scalar('min', tf.reduce_min(var))
             tf.summary.histogram('histogram', var)
 
-def nn_layer(input_tensor, input_dim, output_dim, layer_name, act=activations):
+def nn_layer(input_tensor, input_dim, output_dim, layer_name, act):
     """Reusable code for making a simple neural net layer.
 
     It does a matrix multiply, bias add, and then uses relu to nonlinearize.
@@ -121,71 +89,103 @@ def nn_layer(input_tensor, input_dim, output_dim, layer_name, act=activations):
         return activations
 
 
-def sequential_layers(input, layer_sizes, name_prefix):
+def sequential_layers(input, layer_sizes, name_prefix, activations, final_activation=tf.identity):
     """ Create a series of fully-connected layers.
-
-    :param input:
-    :param layer_sizes:
-    :param name_prefix:
-    :return:
     """
     current_tensor = input
     for idx in range(len(layer_sizes) - 1):
         if idx == len(layer_sizes) - 1:
             current_tensor = nn_layer(current_tensor, layer_sizes[idx], layer_sizes[idx + 1],
                                       name_prefix + str(idx),
-                                      act=tf.identity)  # Note: removed last activation. This needs to be done outside this function. It was problematic with softmax activation since the loss function wants unscaled logits rather than softmaxed values.
+                                      act=final_activation)  # Note: removed last activation. This needs to be done outside this function. It was problematic with softmax activation since the loss function wants unscaled logits rather than softmaxed values.
         else:
             current_tensor = nn_layer(current_tensor, layer_sizes[idx], layer_sizes[idx + 1],
-                                      name_prefix + str(idx))
+                                      name_prefix + str(idx), activations)
     return current_tensor
 
 
-## Define the network
-# Input to network.
-input = tf.placeholder(tf.float32, shape=(None, layer_sizes[0]), name='input')
+if __name__ == "__main__":
+    ## Parse command line options defining the network.
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--layers', nargs='+', type=int,
+                        help='<Required> Define fully-connected layer sizes from input to output', required=True)
+    parser.add_argument('-s', '--savepath', type=str,
+                        help='<Required> File path and name to save the network graph to.',
+                        required=True)
+    parser.add_argument('-lr', '--learnrate', type=float, help='<Optional> Optimizer learning rate (default = 1e-3).',
+                        required=False, default=1e-3)
+    parser.add_argument('-w', '--weightstd', type=float,
+                        help='<Optional> Standard deviation initialization for weights. (default = 0.1).',
+                        required=False,
+                        default=0.1)
+    parser.add_argument('-b', '--biasinit', type=float, help='<Optional> Initialization for biases. (default = 0.1).',
+                        required=False, default=0.1)
+    parser.add_argument('-a', '--activations', type=str,
+                        help='<Optional> Nonlinear activation type (default leaky_relu).',
+                        required=False, default="leaky_relu",
+                        choices=['relu', 'leaky_relu', 'elu', 'sigmoid', 'tanh', 'identity'])
+    parser.add_argument('-ao', '--activationsout', type=str, help='<Optional> Output layer activation.',
+                        required=False, default="identity",
+                        choices=['relu', 'leaky_relu', 'elu', 'sigmoid', 'tanh', 'identity', 'softmax'])
+    parser.add_argument('-ls', '--loss', type=str, help='<Optional> Loss type.',
+                        required=False, default="huber",
+                        choices=['huber', 'mse', 'cross_entropy', 'policy_gradient', 'qlearn'])
+    args = parser.parse_args()
+    # Get user specified options (or defaults).
+    layer_sizes = args.layers
+    init_weight_stddev = args.weightstd
+    init_bias_val = args.biasinit
+    learning_rate = args.learnrate
+    activations = activation_options.get(args.activations)
+    output_activations = activation_options.get(args.activationsout)
+    savepath = args.savepath
+    loss_selection = args.loss
 
-# Output target for training.
-output_target = tf.placeholder(tf.float32, shape=(None, layer_sizes[-1]), name='output_target')  # Some values, same dim as the output layer, used in figuring out the loss.
-scalar_target = tf.placeholder(tf.float32, [None, ], name="scalar_target")  # Some scalar value tied up in figuring out the loss.
+    ## Define the network
+    # Input to network.
+    input = tf.placeholder(tf.float32, shape=(None, layer_sizes[0]), name='input')
 
-preactivation_output = sequential_layers(input, layer_sizes, "fully_connected")
-activated_output = output_activations(preactivation_output, name='output_activation')
+    # Output target for training.
+    output_target = tf.placeholder(tf.float32, shape=(None, layer_sizes[-1]), name='output_target')  # Some values, same dim as the output layer, used in figuring out the loss.
+    scalar_target = tf.placeholder(tf.float32, [None, ], name="scalar_target")  # Some scalar value tied up in figuring out the loss.
 
-# Loss calculation
-if loss_selection == 'cross_entropy':
-    # Pure matching.
-    loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=output_target, logits=preactivation_output, name='loss')
+    preactivation_output = sequential_layers(input, layer_sizes, "fully_connected", activations)
+    activated_output = output_activations(preactivation_output, name='output_activation')
 
-elif loss_selection == 'policy_gradient':
-    neg_log_prob = tf.nn.softmax_cross_entropy_with_logits_v2(labels=output_target, logits=preactivation_output, name='log_prob')
-    loss = tf.reduce_mean(tf.multiply(neg_log_prob, scalar_target), name='loss')
+    # Loss calculation
+    if loss_selection == 'cross_entropy':
+        # Pure matching.
+        loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=output_target, logits=preactivation_output, name='loss')
 
-elif loss_selection == 'qlearn':
-    Q = tf.reduce_sum(tf.multiply(activated_output, output_target), axis=1, name='mask_q_by_action')
-    loss = tf.losses.mean_squared_error(labels=scalar_target, predictions=Q)
-    loss = tf.identity(loss, name='loss')
-    # loss = tf.reduce_mean(tf.square(scalar_target - Q), name='loss')
+    elif loss_selection == 'policy_gradient':
+        neg_log_prob = tf.nn.softmax_cross_entropy_with_logits_v2(labels=output_target, logits=preactivation_output, name='log_prob')
+        loss = tf.reduce_mean(tf.multiply(neg_log_prob, scalar_target), name='loss')
 
-elif loss_selection == 'huber':
-    loss = tf.identity(tf.losses.huber_loss(output_target, activated_output), name='loss')
+    elif loss_selection == 'qlearn':
+        Q = tf.reduce_sum(tf.multiply(activated_output, output_target), axis=1, name='mask_q_by_action')
+        loss = tf.losses.mean_squared_error(labels=scalar_target, predictions=Q)
+        loss = tf.identity(loss, name='loss')
+        # loss = tf.reduce_mean(tf.square(scalar_target - Q), name='loss')
 
-elif loss_selection == 'mse':
-    loss = tf.reduce_mean(tf.square(activated_output - output_target), name='loss')
+    elif loss_selection == 'huber':
+        loss = tf.identity(tf.losses.huber_loss(output_target, activated_output), name='loss')
 
-else:
-    raise ValueError("Unknown loss type: " + loss_selection)
+    elif loss_selection == 'mse':
+        loss = tf.reduce_mean(tf.square(activated_output - output_target), name='loss')
+
+    else:
+        raise ValueError("Unknown loss type: " + loss_selection)
 
 
-output = tf.identity(activated_output, name='output')  # So output gets named correctly in graph definition.
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, name='optimizer')
-train_op = optimizer.minimize(loss, name='train')
+    output = tf.identity(activated_output, name='output')  # So output gets named correctly in graph definition.
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, name='optimizer')
+    train_op = optimizer.minimize(loss, name='train')
 
-loss_scalar = tf.summary.scalar('loss_function', loss)
-tf.summary.histogram('output_distribution', output)
-merged_summary_op = tf.summary.merge_all(name="summary")
+    loss_scalar = tf.summary.scalar('loss_function', loss)
+    tf.summary.histogram('output_distribution', output)
+    merged_summary_op = tf.summary.merge_all(name="summary")
 
-init = tf.global_variables_initializer()
-saver_def = tf.train.Saver().as_saver_def()
-with open(savepath, 'wb') as f:
-    f.write(tf.get_default_graph().as_graph_def().SerializeToString())
+    init = tf.global_variables_initializer()
+    saver_def = tf.train.Saver().as_saver_def()
+    with open(savepath, 'wb') as f:
+        f.write(tf.get_default_graph().as_graph_def().SerializeToString())
